638bf75c00f75a0298e67dca192ab6e8e1ba24ca
CayChance <chance17610999610@gmail.com>
2021-01-22 00:09:46 +0800

    update

--- test.js
+++ test.js
@@ -1,40 +1,16 @@
-function baseClone(target) {
-  if (!isObject(target)) return target;
-  let result = Array.isArray(target) ? [] : {};
-  let keys = Object.keys(target);
-  for (let i = 0, len = keys.length; i < len; i++) {
-    result[keys[i]] = baseClone(target[keys[i]]);
+function quickSort(arr){
+  // 1、从数组arr中拿出中间项mid
+  // 2、声明左右两个数组，循环arr，比mid小push到左数组，否则push到右数组
+  // 3、对左右两个数组进行递归处理，把左数组，中间值，右数组拼在一起
+  // 4、如果数组里面只有一项或者没有，那么原封不动返回数组
+  if(arr.length<=1) return arr
+  let middleIndex = parseInt(arr.length/2)
+  let middleValue = arr.splice(middleIndex, 1)[0]
+  let arrLeft = []
+  let arrRight = []
+  for(let i=0;i<arr.length;i++) {
+    let item = arr[i];
+    item<middleValue ? arrLeft.push(item) : arrRight.push(item)
   }
-  return result;
+  return quickSort(arrLeft).concat(middleValue, quickSort(arrRight))
 }
-function isObject(target) {
-  return target !== null && typeof target === "object";
-}
-
-function deepClone(target) {
-  let visitedMap = new Map();
-  function baseClone(target) {
-    if (!isObject(target)) return target;
-    if (visitedMap.get(target)) return visitedMap.get(target); // 先获取map中是否存在当前target，如果存在则直接返回
-    let result = Array.isArray(target) ? [] : {};
-    visitedMap.set(target, result);
-    let keys = Object.keys(target);
-    for (let i = 0, len = keys.length; i < len; i++) {
-      result[keys[i]] = baseClone(target[keys[i]]);
-    }
-    return result;
-  }
-  return baseClone(target);
-}
-
-
-var arr = [1, 2, 3];
-var obj = {
-  arr1: [],
-  arr2: []
-};
-obj.arr1 = arr;
-obj.arr2 = arr;
-obj.arr1 === obj.arr2; // true
-var cloneObj = baseClone(obj);
-cloneObj.arr1 === cloneObj.arr2; // false
--- utils.js
+++ utils.js
@@ -422,6 +422,22 @@ bubbleSort(arr);
 
 // 插入排序
 // 快速排序
+function quickSort(arr){
+  // 1、从数组arr中拿出中间项mid
+  // 2、声明左右两个数组，循环arr，比mid小push到左数组，否则push到右数组
+  // 3、对左右两个数组进行递归处理，把左数组，中间值，右数组拼在一起
+  // 4、如果数组里面只有一项或者没有，那么原封不动返回数组
+  if(arr.length<=1) return arr
+  let middleIndex = parseInt(arr.length/2)
+  let middleValue = arr.splice(middleIndex, 1)[0]
+  let arrLeft = []
+  let arrRight = []
+  for(let i=0;i<arr.length;i++) {
+    let item = arr[i];
+    item<middleValue ? arrLeft.push(item) : arrRight.push(item)
+  }
+  return quickSort(arrLeft).concat(middleValue, quickSort(arrRight))
+}
 // 设计模式
 // 工厂模式
 // 单例模式
